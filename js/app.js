b5934413c952ce8cbebf196701a288bc5cb61591
pinalef <tierralavista@gmail.com>
2017-12-07 17:35:35 -0300

    subiendo footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-// La funcionalidad de tu proyecto
+$(document).ready(function(){
+  // menu movil
+  $('.button-collapse').sideNav();
+  //modal inicio sesion
+  $('#modal1').modal('open');
+});
+
+
